1fdc643a09edd5b6642cd0ba2a962dccda46a942
owenflatman <owenjflatman@gmail.com>
2021-09-10 18:03:56 -0500

    SmartDashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,7 +28,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
+  // private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
   
   // create constants
@@ -101,7 +101,7 @@ public class Robot extends TimedRobot {
 
 
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
+    m_chooser.addOption("My fasdfasdfP", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
   }
 
@@ -114,6 +114,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
+
+
     // if(abs(driver.getRawAxis(LEFT_X)) > 0.04){
     //   System.out.println(driver.getRawAxis(LEFT_X));
     // }
@@ -267,30 +269,32 @@ public class Robot extends TimedRobot {
     // start timer
     timer.start();
 
-    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+    // System.out.println("Auto selected: " + m_autoSelected);
 
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
+    // switch (m_autoSelected) {
+    //   case kCustomAuto:
+    //     // Put custom auto code here
+    //     break;
+    //   case kDefaultAuto:
+    //   default:
+    //     // Put default auto code here
+    //     break;
+    // }
   }
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
 
+    // SmartDashboard.putNumber
+  }
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
