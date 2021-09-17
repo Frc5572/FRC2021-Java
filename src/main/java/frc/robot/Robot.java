b7a040a8fc3e910c4b1483a03976f6d3c8a319a0
agrinmanriv0537 <75352922+agrinmanriv0537@users.noreply.github.com>
2021-09-17 12:17:48 -0500

    idek

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,6 +23,7 @@ import edu.wpi.first.wpilibj.Compressor;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import frc.robot.constants;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -34,37 +35,14 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  
-  // create constants
-  final int LEFT_Z = 2;
-  final int LEFT_X = 0;
-  final int LEFT_Y = 1;
-  final int RIGHT_Z = 3;
-  final int RIGHT_X = 4;
-  final int RIGHT_Y = 5;
-  final int LEFT_BUMPER = 5;
-  final int RIGHT_BUMPER = 6;
-  final int X_BUTTON = 3;
-  final int Y_BUTTON = 4;
-  final int B_BUTTON = 2;
-  final int A_BUTTON = 1;
-  final int START_BUTTON = 8;
-  final int BACK_BUTTON = 7;
-  final int LEFT_STICK_BUTTON = 9;
-  final int RIGHT_STICK_BUTTON = 10;
-  final int DPadDown = 180;
-  final int DPadUp = 0;
-  final int DPadLeft = 270;
-  final int DPadRight = 90;
-  // final int RIGHT_TRIGGER = 12;
 
+  constants.setConstants();
 
   // public var for shooter PID
   double v1;
   double v2;
   int PCM1 = 0;
   int PCM2 = 1;
-
 
   // initialize timer
   Timer timer = new Timer();
---
+++ src/main/java/frc/robot/constants.java
@@ -0,0 +1,27 @@
+package frc.robot;
+
+public class constants {
+    public void setConstants(){
+        // create constants
+        final int LEFT_Z = 2;
+        final int LEFT_X = 0;
+        final int LEFT_Y = 1;
+        final int RIGHT_Z = 3;
+        final int RIGHT_X = 4;
+        final int RIGHT_Y = 5;
+        final int LEFT_BUMPER = 5;
+        final int RIGHT_BUMPER = 6;
+        final int X_BUTTON = 3;
+        final int Y_BUTTON = 4;
+        final int B_BUTTON = 2;
+        final int A_BUTTON = 1;
+        final int START_BUTTON = 8;
+        final int BACK_BUTTON = 7;
+        final int LEFT_STICK_BUTTON = 9;
+        final int RIGHT_STICK_BUTTON = 10;
+        final int DPadDown = 180;
+        final int DPadUp = 0;
+        final int DPadLeft = 270;
+        final int DPadRight = 90;
+    }
+}
